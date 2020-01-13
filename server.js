9f94b05531714cbe304520a10b0181b6559f0c2e
Tenaimi Okurude <tjokurude@gmail.com>
2020-01-13 17:51:43 -0500

    hotfix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const logger = require('morgan')
 const PORT = 3001
 
 app.use(cors()) //cross origin helper as react can't recognize http
-app.use(logger('dev'))
+app.use(logger('dev')) //to log rest request to server
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -17,15 +17,27 @@ app.get('/', (req, res) => {
     res.send('Welcome to my server')
 })
 
+//adding new user
 app.post('/users/', (req, res) => {
-    console.log(req.body)
+    //console.log(req.body)
     users.push(req.body)
+    res.send(users)
+})
+
+//delete a user
+//very buggy????
+app.delete('/users/', (req, res) => {
+    console.log(req.body)
+    const key = req.body.firstName
+    delete users[key]
+    //users.pop(req.body)
+    res.send(users)
 })
 
 app.get('/users/', async (req, res) => {
-    console.log(users)
+    //console.log(users)
     res.send(users)
 })
 
 app.listen(PORT, () => `server running on port ${PORT}`)
-console.log('got here!!')
+//console.log('got here!!')
